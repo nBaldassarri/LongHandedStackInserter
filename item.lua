bee5db414763b8558ed3a5362edb8faf8e634baa
Nicolas Baldassarri <xnicons98itax@gmail.com>
2022-04-22 14:39:02 +0200

    Setup new inserter

--- item.lua
+++ item.lua
@@ -1,41 +1,46 @@
-local fireArmor = table.deepcopy(data.raw["armor"]["heavy-armor"]) 
--- copy the table that defines the heavy armor item into the fireArmor variable
+local NAME = "long-handed-stack-inserter"
+local ICON = "__factorio-mod-test__/graphics/long-inserter.png"
 
-fireArmor.name = "fire-armor"
-fireArmor.icons = {
-  {
-    icon = fireArmor.icon,
-    tint = {r=1,g=0,b=0,a=0.3}
-  },
-}
+-- ENTITY
 
-fireArmor.resistances = {
+local entity = table.deepcopy(data.raw.inserter["stack-inserter"])
+entity.name = NAME
+entity.icon = ICON
+entity.icon_size = 32
+
+entity.hand_size = data.raw.inserter["long-handed-inserter"].hand_size
+entity.insert_position = data.raw.inserter["long-handed-inserter"].insert_position
+entity.pickup_position = data.raw.inserter["long-handed-inserter"].pickup_position
+entity.platform_picture =
+{
+  sheet =
   {
-    type = "physical",
-    decrease = 6,
-    percent = 10
-  },
-  {
-    type = "explosion",
-    decrease = 10,
-    percent = 30
-  },
-  {
-    type = "acid",
-    decrease = 5,
-    percent = 30
-  },
-  {
-    type = "fire",
-    decrease = 0,
-    percent = 100
+    filename = "__factorio-mod-test__/graphics/ultimate-long-inserter-platform.png",
+    height = 40,
+    priority = "extra-high",
+    shift = {
+      0.03125,
+      0.1875
+    },
+    width = 53
   }
 }
 
-local recipe = table.deepcopy(data.raw["recipe"]["heavy-armor"])
-recipe.enabled = true
-recipe.name = "fire-armor"
-recipe.ingredients = {{"copper-plate",200},{"steel-plate",50}}
-recipe.result = "fire-armor"
+-- ITEM
 
-data:extend{fireArmor,recipe}
+local item = table.deepcopy(data.raw.item["stack-inserter"])
+item.name = NAME
+item.icon = ICON
+item.icon_size = 32
+item.place_result = NAME
+
+-- RECIPE
+
+local recipe = table.deepcopy(data.raw.recipe["stack-inserter"])
+recipe.ingredients = {{"long-handed-inserter", 1}, {"stack-inserter", 1}}
+recipe.enabled = true
+recipe.name = NAME
+recipe.result = NAME
+
+
+data:extend{entity,item,recipe}
